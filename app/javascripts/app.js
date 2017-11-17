915e6dce28e446e5ba06dc0025bb4aa3650a7438
Elliot Shohet <elliot.shohet@gmail.com>
2017-11-17 00:30:48 -0800

    Closer

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -1,52 +1,93 @@
 // Import the page's CSS. Webpack will know what to do with it.
-import "../stylesheets/app.css";
-
+import '../stylesheets/app.css';
+import * as $ from 'jquery';
 // Import libraries we need.
-import { default as Web3} from 'web3';
-import { default as contract } from 'truffle-contract'
+import {default as Web3} from 'web3';
+import {default as contract} from 'truffle-contract';
+import {default as crypto} from 'crypto';
+import {default as eccrypto} from 'eccrypto';
 
 // Import our contract artifacts and turn them into usable abstractions.
-import mail_artifacts from '../../build/contracts/Mail.json'
+import mail_artifacts from '../../build/contracts/Mail.json';
+
+import swal from 'sweetalert2';
+
+require('sweetalert2/dist/sweetalert2.min.css');
 
 // MetaCoin is our usable abstraction, which we'll use through the code below.
-var Mail = contract(mail_artifacts);
-window.App = {
-  start: function() {
-      Mail.setProvider(web3.currentProvider);
-      this.getPublicKey();
-  },
+let Mail = contract(mail_artifacts);
 
-  getPublicKey: async () => {
-    web3.eth.getAccounts(async (err, accounts) => {
-		const account = accounts[0];
-		const mail = await Mail.deployed();
-        const pubKey = await mail.getPub.call(account, {from: account });
-        console.log("Pub key:" , pubKey);
+function allEvents(ev, cb) {
+	ev({}, {fromBlock: '0', toBlock: 'latest'}).get((error, results) => {
+		if (error) return cb(error);
+		results.forEach(result => cb(null, result));
+		ev().watch(cb);
 	});
-  },
+}
 
-  registerPubKey: async() => {
-	  web3.eth.getAccounts(async (err, accounts) => {
-		  const account = accounts[0];
-		  const mail = await Mail.deployed();
-          mail.updatePubRegistry('abc', {from: account});
-	  });
-  }
+window.App = {
+
+	start: () => {
+		Mail.setProvider(web3.currentProvider);
+		App.getPublicKey();
+	},
+
+	getPublicKey: async () => {
+		web3.eth.getAccounts(async (err, accounts) => {
+			const account = accounts[0];
+			const mail = await Mail.deployed();
+			const pubKey = await mail.getPub.call(account, {from: account});
+			$('#public_key').html(pubKey);
+			this.publicKey = pubKey;
+			App.getMail();
+		});
+	},
+	registerPubKey: async (pubKey) => {
+		web3.eth.getAccounts(async (err, accounts) => {
+			const account = accounts[0];
+			const mail = await Mail.deployed();
+			mail.updatePubRegistry(pubKey, {from: account});
+		});
+	},
+	newUser: async() => {
+		const privateKey = crypto.randomBytes(32);
+		const publicKey = eccrypto.getPublic(privateKey);
+		window.localStorage.setItem('private', privateKey);
+		App.registerPubKey(Buffer.from(publicKey).toString('hex'));
+	},
+
+	getMail: async () => {
+		const mail = await Mail.deployed();
+		allEvents(mail.Mail, (err, email) => {
+			console.log(email);
+		});
+	},
+	sendMail: async(to, subject, body) => {
+		//TODO
+	},
+
+	encrypt: async function (msg, publicKey) {
+		//encrypts photo
+		return eccrypto.encrypt(publicKey, new Buffer(msg));
+	},
+	decrypt: function (msg, privateKey) {
+		//decrypts photo
+		return eccrypto.decrypt(privateKey, msg);
+	},
 
 
 };
 
-window.addEventListener('load', function() {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If you find that your accounts don't appear or you have 0 MetaCoin, ensure you've configured that source properly. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider);
-  } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:9545"));
-  }
+window.addEventListener('load', function () {
+	// Checking if Web3 has been injected by the browser (Mist/MetaMask)
+	if (typeof web3 !== 'undefined') {
+		// Use Mist/MetaMask's provider
+		window.web3 = new Web3(web3.currentProvider);
+	} else {
+		console.warn('No web3 detected. Falling back to http://127.0.0.1:9545. You should remove this fallback when you deploy live, as it\'s inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask');
+		// fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+		window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'));
+	}
 
-  App.start();
+	App.start();
 });
