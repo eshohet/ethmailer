cf4caf8599afa2b2aef87e76f4ea8cc95a22cb6a
Elliot Shohet <elliot.shohet@gmail.com>
2017-11-17 06:24:22 -0800

    Lots of updates

--- truffle.js
+++ truffle.js
@@ -1,11 +1,19 @@
-// Allows us to use ES6 in our migrations and tests.
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+const _provider = new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/");
+// console.log(_provider);
 
 module.exports = {
   networks: {
     development: {
-      host: 'localhost',
+      host: "localhost",
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: _provider,
+      network_id: 4
     }
   }
-}
+};
